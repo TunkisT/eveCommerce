3e1dd0e52309a516ae016c9c68c9404bb30b8559
TunkisT <ttunkis@gmail.com>
2025-02-07 20:44:04 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,33 @@
-// const config = require("./src/config/config.json");
+
+const config = require("./src/config/config.json");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  reactStrictMode: true,
+  basePath: config.base_path !== "/" ? config.base_path : "",
+  trailingSlash: config.site.trailing_slash,
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+module.exports = nextConfig;
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   experimental: {
-//     missingSuspenseWithCSRBailout: false,
-//   },
+//   output: "export",  // <=== enables static exports
 //   reactStrictMode: true,
-//   basePath: config.base_path !== "/" ? config.base_path : "",
-//   trailingSlash: config.site.trailing_slash,
-//   output: "export",
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "cdn.shopify.com",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
 // };
 
 // module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
